a256b87aafdae5a4e6952c13fde2e63d5df15abb
Ruthie7910 <wendierut@gmail.com>
2024-06-08 22:30:39 +0100

    Create testchild.py
    Giving my personal thoughts on studying

---
+++ testchild.py
@@ -0,0 +1,5 @@
+## learning something new can be exciting yet scary
+
+### you're like, "what if I don't get this, am I going to spend the rest of my evening studying"
+
+#### nobody really likes to study throughout unless they have a target to meet, or they're expecting to earn from it
